a9bb6650e89a26cd9c47a0415f136ef50fb6f156
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-02 19:58:36 +0600

    all file has been added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCv3NvukwvoiEojgKZ69Lh_kWFpNrhjJOQ",
+  authDomain: "food-chef-ae392.firebaseapp.com",
+  projectId: "food-chef-ae392",
+  storageBucket: "food-chef-ae392.appspot.com",
+  messagingSenderId: "396726956742",
+  appId: "1:396726956742:web:71129324894c0d28701dc7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
