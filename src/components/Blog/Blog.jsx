8cba6b99f587331e479cbcb98aa8f9c9043527bb
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-04 19:51:18 +0600

    Second question added in blog file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -11,16 +11,8 @@ const Blog = () => {
                         <p><span className='fw-bold'>Ans:-</span>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
                     </div>
                     <div className='mb-4'>
-                        <h4 >1.Tell us the differences between uncontrolled and controlled components.?</h4>
-                        <p><span className='fw-bold'>Ans:-</span>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
-                    </div>
-                    <div className='mb-4'>
-                        <h4 >1.Tell us the differences between uncontrolled and controlled components.?</h4>
-                        <p><span className='fw-bold'>Ans:-</span>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
-                    </div>
-                    <div className='mb-4'>
-                        <h4 >1.Tell us the differences between uncontrolled and controlled components.?</h4>
-                        <p><span className='fw-bold'>Ans:-</span>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
+                        <h4 >2.How to validate React props using PropTypes?</h4>
+                        <p><span className='fw-bold'>Ans:-</span>rops and PropTypes are important mechanisms for passing read-only attributes between React components.We can use React props, short for properties, to send data from one component to another. If a component receives the wrong type of props, it can cause bugs and unexpected errors in your app.</p>
                     </div>
                 </div>
             </div>
