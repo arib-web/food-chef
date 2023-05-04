471b4a34e7da4fde3263af70aa91a19c49049ac7
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-04 19:51:49 +0600

    Third question added in blog file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -14,6 +14,10 @@ const Blog = () => {
                         <h4 >2.How to validate React props using PropTypes?</h4>
                         <p><span className='fw-bold'>Ans:-</span>rops and PropTypes are important mechanisms for passing read-only attributes between React components.We can use React props, short for properties, to send data from one component to another. If a component receives the wrong type of props, it can cause bugs and unexpected errors in your app.</p>
                     </div>
+                    <div className='mb-4'>
+                        <h4 >3.Tell us the difference between nodejs and express js.?</h4>
+                        <p><span className='fw-bold'>Ans:-</span><span className="fw-bold">NodeJS</span> is the package, which provides the JavaScript run-time environment, whereas <span className='fw-bold'>Express</span> is a framework that sits on top of NodeJS and helps us to handle requests and responses.</p>
+                    </div>
                 </div>
             </div>
         </div>
