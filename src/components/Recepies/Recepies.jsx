524c381d8d75d521ba65d7cae240fa96a5aef9ab
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-04 18:19:38 +0600

    A chef total bio data has been added

--- src/components/Recepies/Recepies.jsx
+++ src/components/Recepies/Recepies.jsx
@@ -4,19 +4,7 @@ const Recepies = ({recepie}) => {
     const {recipe_name}=recepie;
     console.log(recepie);
     return (
-        <div className='col-md-4'>
-            <div className="card">
-                <img  className="card-img-top" />
-                <div className="card-body">
-                    <h5 className="card-title text-center">{recipe_name}</h5>
-                    <p>Exper years</p>
-                    <p>Number of rees</p>
-                    <div className='d-flex align-items-center justify-content-between'>
-                        <p ><span className='fw-bold'> Likes:- </span></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     );
 };
 
